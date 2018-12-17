22c9d14dbf2126ee934de5fee96dab044dc82a25
kindkarthik <vivekiyer98@gmail.com>
2018-12-17 16:04:10 +0530

    karthik changes

--- resources/views/overview.blade.php
+++ resources/views/overview.blade.php
@@ -425,7 +425,10 @@ display: table-cell;
 	 
 }
 
+.active {
 
+  background-color: red;
+}
 
 
 
@@ -435,7 +438,66 @@ display: table-cell;
 
 </head>
 <body onload="init()">
-@include('mynav')
+<nav class="navbar navbar-default" style="background-color: rgb(35,162,218);">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="/" style="color: white">Home</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="{{ url('/newapplicant') }}"  target="iframe" style="color: white">New Applicant</a></li>
+        <li><a href="{{ url('/listapplicant') }}" target="iframe" style="color: white">Applicants List</a></li>
+        <li>
+           <div class="dropdown">
+             <button class="dropbtn tag">Banks
+               <i class="fa fa-caret-down"></i>
+             </button>
+              <div class="dropdown-content">
+                 <a  href="{{ url('/new_bank') }}">New Bank</a>
+                 <a  href="{{ url('/list_bank') }}">Bank Detail</a>
+                 
+              </div>
+          </div> 
+        </li>
+        <li>
+          <div class="dropdown">
+             <button class="dropbtn ">Savings
+               <i class="fa fa-caret-down"></i>
+             </button>
+              <div class="dropdown-content">
+                 <a href="{{ url('/new_saving') }}" >ADD A SAVINGS ACCOUNT</a>
+                 <a href="{{ url('/list_savings') }}">ACCOUNTS' DETAILS</a>
+                 
+              </div>
+        </li>
+        <li><div class="dropdown">
+             <button class="dropbtn">Transactions
+               <i class="fa fa-caret-down"></i>
+             </button>
+              <div class="dropdown-content">
+                 <a  href="{{ url('/new_fd') }}">ADD FD ACCOUNT</a>
+                 <a  href="{{ url('/new_expense') }}">LOCAL EXPENSE</a>
+                 <a  href="{{ url('/list_disbursements') }}">SANCTION DETAILS</a>
+                 <a  href="{{ url('/fd_list') }}">FD LIST</a>
+                 
+              </div></li>
+        <li><a  href="{{ url('/aboutus') }}"  style="color: white">About Us</a></li>
+        <li><a  href="{{ url('/index') }}"  style="color: white">View Status</a></li>
+        <li><a class="active" href="{{ url('/overview') }}"  style="color: white">Overview</a></li>
+       
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="{{ url('/home') }}" style="color: white"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
 
 <?php
 $acc_values= Expense::select('acc_balance')
